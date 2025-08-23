52548942607b5fe0a0ea37aa1a5e033889c228d1
ccrayp <demixhd2@gmail.com>
2025-08-23 10:55:35 +0300

    fix project page

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -55,6 +55,7 @@ export default function Posts() {
                         <Button onClick={() => handleShowModal(null)}>Добавить запись</Button>
                     </Col>
                 </Row>
+                <ModalProject showModal={showModal} handleCloseModal={handleCloseModal} handleOnSuccess={handleOnSuccess} id={idToEdit} imgPrefix={imgPrefix} />
             </Container>
         )
     }
